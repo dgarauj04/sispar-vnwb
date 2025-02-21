01cc54f694ad2d6821ff57e57c479e88d78968e7
Douglas Araujo <douglasdab10@gmail.com>
2025-02-21 15:51:59 -0300

    Update InformArea.jsx

--- src/components/request/informarea/InformArea.jsx
+++ src/components/request/informarea/InformArea.jsx
@@ -1,370 +1,81 @@
 import React, { useState } from 'react';
-import styles from './InputArea.module.scss';
-import { FaCalendarAlt, FaChevronDown } from 'react-icons/fa';
-import { FiPlus, FiDelete } from 'react-icons/fi';
-import MessageForm from './messageForm/MessageForm';
+import styles from './InformArea.module.scss';
+import { RiDeleteBin6Line } from 'react-icons/ri';
+import { BiSolidFile } from 'react-icons/bi';
+import MessageDelete from './messageDelete/MessageDelete';
 
-export default function InputArea({ onAddReembolso }) {
-  const [formData, setFormData] = useState({
-    nome: '',
-    empresa: '',
-    prestacaoContas: '',
-    descricao: '',
-    data: '',
-    tipoDespesa: '',
-    controleCusto: '',
-    ordInt: '',
-    pep: '',
-    div: '',
-    distKm: '',
-    moeda: '',
-    valorKm: '',
-    valTaxa: '',
-    valFaturado: '',
-  });
-
-  const [showResetMessage, setShowResetMessage] = useState(false);
-
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setFormData({ ...formData, [name]: value });
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const novoReembolso = {
-      id: Date.now(),
-      colaborador: formData.nome,
-      empresa: formData.empresa,
-      prestacaoContas: formData.prestacaoContas,
-      data: formData.data,
-      motivo: formData.descricao,
-      tipoReembolso: formData.tipoDespesa,
-      controleCusto: formData.controleCusto,
-      ordInt: formData.ordInt,
-      div: formData.div,
-      pep: formData.pep,
-      moeda: formData.moeda,
-      distKm: formData.distKm,
-      valorKm: formData.valorKm,
-      valorFaturado: formData.valFaturado,
-      despesa: formData.valTaxa,
-    };
-    onAddReembolso(novoReembolso);
-    setFormData({
-      nome: '',
-      empresa: '',
-      prestacaoContas: '',
-      descricao: '',
-      data: '',
-      tipoDespesa: '',
-      controleCusto: '',
-      ordInt: '',
-      pep: '',
-      div: '',
-      distKm: '',
-      moeda: '',
-      valorKm: '',
-      valTaxa: '',
-      valFaturado: '',
-    });
-  };
-
-  const handleReset = () => {
-    setShowResetMessage(true);
-  };
-
-  const handleConfirmReset = () => {
-    setFormData({
-      nome: '',
-      empresa: '',
-      prestacaoContas: '',
-      descricao: '',
-      data: '',
-      tipoDespesa: '',
-      controleCusto: '',
-      ordInt: '',
-      pep: '',
-      div: '',
-      distKm: '',
-      moeda: '',
-      valorKm: '',
-      valTaxa: '',
-      valFaturado: '',
-    });
-    setShowResetMessage(false);
-  };
-
-  const handleCancelReset = () => {
-    setShowResetMessage(false);
+export default function InformArea({ reembolsos, onRemoveReembolso, deleteMessage, onConfirm, onCancel }) {
+  const deleteClick = (id) => {
+    onRemoveReembolso(id);
   };
 
   return (
     <>
-        <form className={styles.formContainer} onSubmit={handleSubmit}>
-            <div className={styles.firstInputs}>
-              <div className={styles.inputName}>
-                <label htmlFor="nome" className={styles.label}>
-                  Nome Completo
-                </label>
-                <input
-                  type="text"
-                  id="nome"
-                  name="nome"
-                  value={formData.nome}
-                  onChange={handleChange}
-                />
-              </div>
+      <section className={styles.overflowXAuto}>
+        <table className={styles.table}>
+          <thead>
+            <tr className={styles.headerRow}>
+              <th className={styles.headerElement}></th>
+              <th className={styles.headerElement}>Colaborador(a)</th>
+              <th className={styles.headerElement}>Empresa</th>
+              <th className={styles.headerElement}>Nº Prest.</th>
+              <th className={styles.headerElement}>Data</th>
+              <th className={styles.headerElement}>Motivo</th>
+              <th className={styles.headerElement}>Tipo Reemb.</th>
+              <th className={styles.headerElement}>Ctr. Custo</th>
+              <th className={styles.headerElement}>Ord. Int.</th>
+              <th className={styles.headerElement}>Div.</th>
+              <th className={styles.headerElement}>PEP</th>
+              <th className={styles.headerElement}>Moeda</th>
+              <th className={styles.headerElement}>Dist. Km</th>
+              <th className={styles.headerElement}>Val. Km</th>
+              <th className={styles.headerElement}>Val. Faturado</th>
+              <th className={styles.headerElement}>Despesa</th>
+            </tr>
+          </thead>
 
-              <div className={styles.inputEmpresa}>
-                <label htmlFor="empresa" className={styles.label}>
-                  Empresa
-                </label>
-                <input
-                  type="text"
-                  id="empresa"
-                  name="empresa"
-                  value={formData.empresa}
-                  onChange={handleChange}
-                />
-              </div>
-
-              <div className={styles.inputPrestacao}>
-                <label htmlFor="prestacaoContas" className={styles.label}>
-                  N° Prest.Contas
-                </label>
-                <input
-                  type="text"
-                  id="prestacaoContas"
-                  name="prestacaoContas"
-                  value={formData.prestacaoContas}
-                  onChange={handleChange}
-                />
-              </div>
-            
-              <div className={styles.inputDescricao}>
-                <label htmlFor="descricao" className={styles.label}>
-                  Descrição / Motivo do Reembolso
-                </label>
-                <input
-                  type="text"
-                  id="descricao"
-                  name="descricao"
-                  value={formData.descricao}
-                  onChange={handleChange}
-                ></input>
-              </div>
-            </div>
-
-        <div className={styles.separatorLine}></div>
-
-            <div className={styles.secondInputs}>
-              <div className={styles.inputDate}>
-                <label htmlFor="data" className={styles.label}>
-                  Data
-                </label>
-                <div className={styles.inputWrapper}>
-                  <input
-                    type="date"
-                    id="data"
-                    name="data"
-                    value={formData.data}
-                    onChange={handleChange}
-                  />
-                  <div className={styles.inputIcon}>
-                    <FaCalendarAlt style={{ marginBottom: '10px' }} />
-                  </div>
-                </div>
-              </div>
-
-              <div className={styles.selectDespesa}>
-                <label htmlFor="tipoDespesa" className={styles.label}>
-                  Tipo de Despesa
-                </label>
-                <div className={styles.inputWrapper}>
-                  <select
-                    id="tipoDespesa"
-                    name="tipoDespesa"
-                    value={formData.tipoDespesa}
-                    onChange={handleChange}
+          <tbody>
+            {reembolsos.map((reembolso) => (
+              <tr key={reembolso.id}>
+                <td className={styles.ElementButton}>
+                  <button
+                    className={styles.deleteButton}
+                    aria-label="Deletar"
+                    onClick={() => deleteClick(reembolso.id)}
                   >
-                    <option value="" disabled selected>
-                      Selecionar
-                    </option>
-                    <option value="alimentacao">Alimentação</option>
-                    <option value="combustivel">Combustível</option>
-                    <option value="conducao">Condução</option>
-                    <option value="estacionamento">Estacionamento</option>
-                    <option value="viagemAdmin">Viagem admin.</option>
-                    <option value="viagemOperacional">
-                      Viagem operacional
-                    </option>
-                    <option value="eventosRepresentacao">
-                      Eventos de representação
-                    </option>
-                  </select>
-                  <div className={styles.inputIconDespesa}>
-                    <FaChevronDown />
-                  </div>
-                </div>
-              </div>
+                    <RiDeleteBin6Line
+                      className={styles.iconDelete}
+                      aria-hidden="true"
+                    />
+                  </button>
+                </td>
+                <td className={styles.Element}>{reembolso.colaborador}</td>
+                <td className={styles.Element}>{reembolso.empresa}</td>
+                <td className={styles.Element}>{reembolso.prestacaoContas}</td>
+                <td className={styles.Element}>{reembolso.data}</td>
+                <td className={styles.ElementText}>
+                  <BiSolidFile className={styles.iconText} aria-hidden="true" />
+                </td>
+                <td className={styles.Element}>{reembolso.tipoReembolso}</td>
+                <td className={styles.Element}>{reembolso.controleCusto}</td>
+                <td className={styles.Element}>{reembolso.ordInt}</td>
+                <td className={styles.Element}>{reembolso.div}</td>
+                <td className={styles.Element}>{reembolso.pep}</td>
+                <td className={styles.Element}>{reembolso.moeda}</td>
+                <td className={styles.Element}>{reembolso.distKm}</td>
+                <td className={styles.Element}>{reembolso.valorKm}</td>
+                <td className={styles.Element}>{reembolso.valorFaturado}</td>
+                <td className={styles.Element}>{reembolso.despesa}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </section>
 
-              <div className={styles.selectControl}>
-                <label htmlFor="controleCusto" className={styles.label}>
-                  Controle de Custo
-                </label>
-                <div className={styles.inputWrapper}>
-                  <select
-                    id="controleCusto"
-                    name="controleCusto"
-                    value={formData.controleCusto}
-                    onChange={handleChange}
-                  >
-                    <option value="" disabled selected>
-                      Selecionar
-                    </option>
-                    <option value="1100109002">
-                      1100109002 - FIN CONTROLES INTERNOS MTZ
-                    </option>
-                    <option value="1100110002">
-                      1100110002 - FIN VICE-PRESIDENCIA FINANCAS MTZ
-                    </option>
-                    <option value="1100110102">
-                      1100110102 - FIN CONTABILIDADE MTZ
-                    </option>
-                  </select>
-                  <div className={styles.inputIconControle}>
-                    <FaChevronDown />
-                  </div>
-                </div>
-              </div>
-
-              <div className={styles.inputOrd}>
-                <label htmlFor="ordInt" className={styles.label}>
-                  Ord. Int.
-                </label>
-                <input
-                  type="number"
-                  id="ordInt"
-                  name="ordInt"
-                  value={formData.ordInt}
-                  onChange={handleChange}
-                />
-              </div>
-
-              <div className={styles.inputPep}>
-                <label htmlFor="pep" className={styles.label}>
-                  PEP
-                </label>
-                <input
-                  type="number"
-                  id="pep"
-                  name="pep"
-                  value={formData.pep}
-                  onChange={handleChange}
-                />
-              </div>
-
-              <div className={styles.inputDiv}>
-                <label htmlFor="div" className={styles.label}>
-                  Div.
-                </label>
-                <input
-                  type="number"
-                  id="div"
-                  name="div"
-                  value={formData.div}
-                  onChange={handleChange}
-                  className={styles.inputDiv}
-                />
-              </div>
-
-              <div className={styles.inputDist}>
-                <label htmlFor="distKm" className={styles.label}>
-                  Dist. / Km
-                </label>
-                <input
-                  type="text"
-                  id="distKm"
-                  name="distKm"
-                  value={formData.distKm}
-                  onChange={handleChange}
-                />
-              </div>
-
-              <div className={styles.inputMoeda}>
-                <label htmlFor="moeda" className={styles.label}>
-                  Moeda
-                </label>
-                <input
-                  type="text"
-                  id="moeda"
-                  name="moeda"
-                  value={formData.moeda}
-                  onChange={handleChange}
-                />
-              </div>
-
-              <div className={styles.inputValorKm}>
-                <label htmlFor="valorKm" className={styles.label}>
-                  Valor / Km
-                </label>
-                <input
-                  type="number"
-                  id="valorKm"
-                  name="valorKm"
-                  value={formData.valorKm}
-                  onChange={handleChange}
-                />
-              </div>
-
-              <div className={styles.inputTaxa}>
-                <label htmlFor="valTaxa" className={styles.label}>
-                  Val. Taxa
-                </label>
-                <input
-                  type="number"
-                  id="valTaxa"
-                  name="valTaxa"
-                  value={formData.valTaxa}
-                  onChange={handleChange}
-                />
-              </div>
-
-              <div className={styles.inputFaturado}>
-                <label htmlFor="valFaturado" className={styles.label}>
-                  Val. Faturado
-                </label>
-                <input
-                  type="number"
-                  id="valFaturado"
-                  name="valFaturado"
-                  value={formData.valFaturado}
-                  onChange={handleChange}
-                />
-              </div>
-
-              <div className={styles.buttonContainer}>
-                <button type="submit" className={styles.buttonSave}>
-                  <FiPlus className={styles.iconButton} /> Salvar
-                </button>
-
-                <button
-                  type="reset"
-                  className={styles.buttonClean}
-                  onClick={handleReset}
-                >
-                  <FiDelete className={styles.iconButton} />
-                </button>
-              </div>
-            </div>
-          
-        </form>
-
-      {showResetMessage && (
-        <MessageForm
-          onConfirm={handleConfirmReset}
-          onCancel={handleCancelReset}
+      {deleteMessage && (
+        <MessageDelete
+          onConfirm={onConfirm}
+          onCancel={onCancel}
         />
       )}
     </>
