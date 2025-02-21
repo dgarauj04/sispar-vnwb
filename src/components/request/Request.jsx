7384436bc5067ca601679c341e5e48d15e76d3f0
Douglas Araujo <douglasdab10@gmail.com>
2025-02-21 16:45:32 -0300

    Update Request.jsx

--- src/components/request/Request.jsx
+++ src/components/request/Request.jsx
@@ -9,9 +9,8 @@ import {
   addReembolso,
   removeReembolso,
 } from '../../../api-data/api';
-import MessageCancel from './messageCancel/MessageCancel'
+import MessageCancel from './messageCancel/MessageCancel';
 import { Link } from 'react-router-dom';
-
 
 export default function Request() {
   const [reembolsos, setReembolsos] = useState([]);
@@ -44,34 +43,34 @@ export default function Request() {
   };
 
   const clickCancelRequest = () => {
-    setShowCancelMessage(true); 
+    setShowCancelMessage(true);
   };
 
   const actionConfirmCancel = () => {
-    setShowCancelMessage(false); 
+    setShowCancelMessage(false);
   };
 
   const actionCancelCancel = () => {
-    setShowCancelMessage(false); 
+    setShowCancelMessage(false);
   };
 
   return (
     <>
       <div className={styles.RequestContainer}>
-      <header className={styles.headerNav}>
-            <nav className={styles.navarea}>
-                <Link to="/" className={styles.linkIcon}>
-                    <GoHome />
-                </Link>
-                <span className={styles.separator}>&gt;</span>
-                <Link to="/refund" className={styles.link}>
-                    Reembolsos
-                </Link>
-                <span className={styles.separator}>&gt;</span>
-                <Link to="/request" className={styles.link}>
-                    Solicitação de Reembolso
-                </Link>
-            </nav>
+        <header className={styles.headerNav}>
+          <nav className={styles.navarea}>
+            <Link to="/" className={styles.linkIcon}>
+              <GoHome />
+            </Link>
+            <span className={styles.separator}>&gt;</span>
+            <Link to="/refund" className={styles.link}>
+              Reembolsos
+            </Link>
+            <span className={styles.separator}>&gt;</span>
+            <Link to="/request" className={styles.link}>
+              Solicitação de Reembolso
+            </Link>
+          </nav>
         </header>
 
         <main className={styles.content}>
@@ -86,43 +85,43 @@ export default function Request() {
         </main>
 
         <footer className={styles.footerContainer}>
-          <div className={styles.flexInput}>
-            <div>
-              <label className={styles.labelTotal}>Total Faturado</label>
-              <input
-                type="text"
-                className={styles.inputTotal}
-                value="0.00"
-                readOnly
-              />
-            </div>
-
-            <div>
-              <label className={styles.labelTotal}>Total Despesa</label>
-              <input
-                type="text"
-                className={styles.inputTotal}
-                value="0.00"
-                readOnly
-              />
-            </div>
+ <div className={styles.inputFooter}>
+          <div className={styles.inputTotal}>
+            <label className={styles.labelTotal}>Total Faturado</label>
+            <input
+              type="text"
+              value="0.00"
+              readOnly
+            />
           </div>
 
-          <div className={styles.flexButton}>
+          <div className={styles.inputTotal}>
+            <label className={styles.labelTotal}>Total Despesa</label>
+            <input
+              type="text"
+              value="0.00"
+              readOnly
+            />
+          </div>
+
+         <div className={styles.areabtn}>
             <button className={styles.sendButton}>
               <MdCheck className={styles.iconButton} /> Enviar para Análise
             </button>
-            <button className={styles.cancelButton}  onClick={clickCancelRequest}>
+            <button
+              className={styles.cancelButton}
+              onClick={clickCancelRequest}
+            >
               <MdClose className={styles.iconButton} /> Cancelar Solicitação
             </button>
-          </div>
+         </div>
+         </div>
         </footer>
       </div>
-
       {showCancelMessage && (
         <MessageCancel
-          onConfirm={actionConfirmCancel} 
-          onCancel={actionCancelCancel} 
+          onConfirm={actionConfirmCancel}
+          onCancel={actionCancelCancel}
         />
       )}
     </>
