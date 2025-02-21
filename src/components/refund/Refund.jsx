f0d5cc57594de4edb015e88b60900906461eb767
Douglas Araujo <douglasdab10@gmail.com>
2025-02-21 18:15:18 -0300

    Update Refund.jsx

--- src/components/refund/Refund.jsx
+++ src/components/refund/Refund.jsx
@@ -19,80 +19,82 @@ export default function Refund() {
 
   return (
     <>
-    <div className={styles.content}>
-         <header className={styles.headerNav}>
-            <nav className={styles.navarea}>
-                <Link to="/" className={styles.linkIcon}>
-                    <GoHome />
-                </Link>
-                <span className={styles.separator}>&gt;</span>
-                <Link to="/refund" className={styles.link}>
-                    Reembolsos
-                </Link>
-            </nav>
-         </header>
+      <div className={styles.content}>
+        <header className={styles.headerNav}>
+          <nav className={styles.navarea}>
+            <Link to="/" className={styles.linkIcon}>
+              <GoHome />
+            </Link>
+            <span className={styles.separator}>&gt;</span>
+            <Link to="/refund" className={styles.link}>
+              Reembolsos
+            </Link>
+          </nav>
+        </header>
 
-      <main className={styles.refoundContainer}>
-        <section>
-          <h1 className={styles.title}>Sistema de Reembolso</h1>
-          <p className={styles.subtitle}>
-            Solicite novos pedidos de reembolso, visualize solicitações em
-            análise e todo o histórico.
-          </p>
+        <main className={styles.refoundContainer}>
+          <section className={styles.sectionContent}>
+            <h1 className={styles.title}>Sistema de Reembolso</h1>
+            <p className={styles.subtitle}>
+              Solicite novos pedidos de reembolso, visualize solicitações em
+              análise e todo o histórico.
+            </p>
 
-          <div className={styles.cardsContainer}>
-            <button className={styles.card} onClick={goToRequestPage}>
-              <ClipBoard className={styles.cardIcon} />
-              <h2>Solicitar Reembolso</h2>
-            </button>
+            <div className={styles.cardsContainer}>
+              <button className={styles.card} onClick={goToRequestPage}>
+                <ClipBoard className={styles.cardIcon} />
+                <h2>Solicitar Reembolso</h2>
+              </button>
 
-            <button className={styles.card}>
-              <IoIosList className={styles.cardIcon} />
-              <h2>Verificar Análise</h2>
-            </button>
+              <button className={styles.card}>
+                <IoIosList className={styles.cardIcon} />
+                <h2>Verificar Análise</h2>
+              </button>
 
-            <button className={styles.card}>
-              <VscHistory className={styles.cardIcon} />
-              <h2>Histórico</h2>
-            </button>
-          </div>
+              <button className={styles.card}>
+                <VscHistory className={styles.cardIcon} />
+                <h2>Histórico</h2>
+              </button>
+            </div>
 
-          <ul className={styles.infoContainer}>
-            <li className={styles.infoItem}>
-              <PiArrowBendUpLeftBold
-                className={`${styles.infoIcon} ${styles.iconSolic}`}
-              />
+            <ul className={styles.infoContainer}>
+              <li className={styles.infoItem}>
+                <PiArrowBendUpLeftBold
+                  className={`${styles.infoIcon} ${styles.iconSolic}`}
+                />
                 <h3>182</h3>
                 <p>Solicitados</p>
-            </li>
+              </li>
 
-            <li className={styles.infoItem}>
-              <IoMdTime className={`${styles.infoIcon} ${styles.iconAnalit}`} />
-                <h3>74</h3> 
+              <li className={styles.infoItem}>
+                <IoMdTime
+                  className={`${styles.infoIcon} ${styles.iconAnalit}`}
+                />
+                <h3>74</h3>
                 <p>Em análise</p>
-            </li>
+              </li>
 
-            <li className={styles.infoItem}>
-              <MdDownloadDone
-                className={`${styles.infoIcon} ${styles.iconAprove}`}
-              />
-                <h3>195</h3> 
+              <li className={styles.infoItem}>
+                <MdDownloadDone
+                  className={`${styles.infoIcon} ${styles.iconAprove}`}
+                />
+                <h3>195</h3>
                 <p>Aprovados</p>
-            </li>
+              </li>
 
-            <li className={styles.infoItem}>
-              <IoMdClose
-                className={`${styles.infoIcon} ${styles.iconReject}`}
-              />
+              <li className={styles.infoItem}>
+                <IoMdClose
+                  className={`${styles.infoIcon} ${styles.iconReject}`}
+                />
                 <h3>41</h3> <p>Rejeitados</p>
-            </li>
-          </ul>
+              </li>
+            </ul>
 
-          <p className={styles.updateText}>
-            <IoCloudDoneOutline /> Sistema atualizado
-          </p>
-        </section>
-      </main>
+            <p className={styles.updateText}>
+              <IoCloudDoneOutline /> Sistema atualizado
+            </p>
+          </section>
+        </main>
       </div>
     </>
   );
